400cccd22a464f4a44aa2bb9eb3f08738c55fa50
FFouetil <fabien.fouetillou@gmail.com>
2016-01-27 17:30:51 +0100

    c1

---
+++ PsychedeSheep/Assets/CameraExt.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraExt : MonoBehaviour {
+
+	public Camera cam;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cam.transform.LookAt(target);
+	}
+}
